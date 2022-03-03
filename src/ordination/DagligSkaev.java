6d2bdb64613606b500a4785e9233f7ce7be8d7ec
Kaare Skau <kaare3014@gmail.com>
2022-03-03 14:22:26 +0100

    ----

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,27 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public  abstract class DagligSkaev extends Ordination {
     // TODO
+    private ArrayList<Dosis> dosisser = new ArrayList<>();
 
-
-    public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, Patient patient, Laegemiddel laegemiddel) {
+        super(startDen, slutDen, patient, laegemiddel);
     }
+
+    public ArrayList<Dosis> getDoser(){
+        return new ArrayList<>(dosisser);
+    }
+    public void opretDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid,antal);
+        dosisser.add(dosis);
+    }
+    public void removeDosis(Dosis dosis) {
+        if(dosisser.contains(dosis))
+            dosisser.remove(dosis);
+    }
+
 }
